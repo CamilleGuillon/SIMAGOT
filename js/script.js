a8567ba18b0b451d7fa65a4764cdb5aba0215e97
CamilleGuillon <35732909+CamilleGuillon@users.noreply.github.com>
2018-04-30 09:44:13 +0200

    menu opérationnel

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-$( document ).ready(function() {
-$(".logo").click(function(){
-  valeur=$("#menu").css("display");
- if($windSize<600)AND(valeur=="none"){
-    $("#menu").css("display","flex");
+$(document).ready(function() {
+
+
+/*Annimation quand click sur bouton su menu*/
+$('.container').click(function() {
+$(this).toggleClass("change");
+$width =$(".menu").css("width");
+console.log($width);
+
+  if ($width ==="0px"){
+   $(".menu").css("width","100%");
+  }else{
+    $(".menu").css("width","0%");
   }
-  })
 
 });
+
+
+
+
+});
+
 
